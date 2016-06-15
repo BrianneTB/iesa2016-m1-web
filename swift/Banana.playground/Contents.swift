f5b57e8a7df733dd7ed2ce4599484bb4fe5791b4
Mastere 1 IT <mastere1it@FRO-206-31.fro-206.froment.in.iesanetwork.com>
2016-06-15 15:02:32 +0200

    #2 array and dictionnaries

--- swift/Banana.playground/Contents.swift
+++ swift/Banana.playground/Contents.swift
@@ -33,3 +33,32 @@ let apples = 3
 let oranges = 5
 let appleSummary = "I have \(apples) apples."
 let fruits = "I have \(apples + oranges) fruits"
+
+//06
+
+var shoppingList = ["catfish", "water", "tulips"]
+shoppingList[1] = "bottle of water" // 0 based indice
+var occupations = [
+    "Malcom" : "Captain",
+    "Kaylee" : "Mechanic" ]
+
+occupations["Jayne"] = "Public relations"
+shoppingList[10] = "too big?"
+var strangeList = ["catfish", 33, 22.2]
+strangeList[1]
+
+//07
+
+var emptyArray = [String]()
+emptyArray.append("Hello")
+emptyArray.append(33) //
+let emptyDicionnary = [String : Float]()
+
+//07.bis
+
+var emptyArray2 = []
+emptyArray2.append(33) //
+emptyArray2.append("Hello")
+
+
+
