9004c0bb642d1c182d0b3af92c51a423970d51ee
Mastere 1 IT <mastere1it@FRO-206-31.fro-206.froment.in.iesanetwork.com>
2016-06-15 14:25:38 +0200

    #2 constantes et variables

---
+++ swift/Banana.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//01
+
+print("Hello world!")
+
+
+//02 variable
+
+var myVariable = 42
+myVariable = 50
+var myOtherVariable :Double
+
+//03 constante
+
+let implicitInteger = 70
+let impliciteDouble = 70.0
+let explicitDouble :Double = 70
