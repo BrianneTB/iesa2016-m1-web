285586b55433e45148478ef18724a1a44cddcb59
Mastere 1 IT <mastere1it@FRO-206-31.fro-206.froment.in.iesanetwork.com>
2016-06-15 14:38:54 +0200

    #2 string

--- swift/Banana.playground/Contents.swift
+++ swift/Banana.playground/Contents.swift
@@ -20,3 +20,16 @@ var myOtherVariable :Double
 let implicitInteger = 70
 let impliciteDouble = 70.0
 let explicitDouble :Double = 70
+
+//04 "6".toInt()!
+
+let label = "The width is "
+let width = 94
+let widthLabel = label + String(width)
+
+//05
+
+let apples = 3
+let oranges = 5
+let appleSummary = "I have \(apples) apples."
+let fruits = "I have \(apples + oranges) fruits"
