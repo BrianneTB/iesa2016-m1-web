b94d50f08d003d71b6331d622b96d5b40866154d
Mastere 1 IT <mastere1it@iMacdeAutres104.sto-002.saint-augustin.in.iesanetwork.com>
2016-06-20 14:20:50 +0200

    #6 opp excercice triforce

--- swift/banana2.playground/Contents.swift
+++ swift/banana2.playground/Contents.swift
@@ -6,6 +6,29 @@ class Shape {
     
     func getInfo() -> String {
         
-        return "A shape with \(sidesCouny) sides."
+        return "A shape with \(sidesCount) sides."
     }
 }
+var shapes = Shape()
+
+
+//24
+
+var shape = Shape()
+
+shape.sidesCount = 7
+
+var shapeDescription = shape.getInfo()
+
+
+//25
+
+class NamedShape {
+    
+    var sidesCount: Int = 0
+    var name: String
+    init(name: String) { self.name = name }
+    func getInfo() -> String { return "\(sidesCount) sides" }
+    
+}
+var carre = NamedShape(name: "drapeau")
